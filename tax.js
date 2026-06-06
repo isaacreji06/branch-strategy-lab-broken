b7999cf4b82586f7166218c5bab2eda5c94cd4ae
Isaac Reji <isaacreji@Isaacs-MacBook-Pro.local>
2026-06-06 09:28:50 +0530

    feat: change tax to 20%

--- tax.js
+++ tax.js
@@ -1,7 +1,7 @@
 function calculateTax(subtotal) {
   // Basic tax calculation
   console.log("Subtotal for tax:", subtotal);
-  return subtotal * -0.05; // INTENTIONALLY BROKEN: Negative tax? 
+  return subtotal * 0.20;
 }
 
 module.exports = calculateTax;
